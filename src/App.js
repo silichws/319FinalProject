5e7c139bb45bc2e292c69c2796733c73654911d7
silwil20 <silichws@gmail.com>
2023-11-03 09:47:18 -0500

    things

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import "./App.css";
+// import logo from "./logo.png";
+import React, { useState } from "react";
+import { Products } from "./Products";
+
+function App() {
+  return <div>{Products[0].description}</div>;
+}
+
+export default App;
