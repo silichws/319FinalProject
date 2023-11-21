75b2195b533116cf30cbdb020f400d5259551460
silwil20 <silichws@gmail.com>
2023-11-21 15:04:03 -0600

    set up

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React, { useState } from "react";
+import { Products } from "./Products";
+
+const App = () => {
+  
+
+  return (
+    <div> 
+      <h1>hello world</h1>
+      
+    </div>
+  );
+};
+
+export default App;
